9acaddcdf0b6e887cc641ac024b2468526ecc139
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-05 20:10:52 -0500

    migrations updated

--- migrations/20201207185121-initial-table-creation.js
+++ migrations/20201207185121-initial-table-creation.js
@@ -1,28 +1,45 @@
 module.exports = {
-  up: queryInterface => queryInterface.bulkInsert('heroes', [
-    {
-      name: 'Captain America',
-      realname: 'Steve Rogers',
-      firstappearance: 'Captain America Comics #1',
-      slug: 'captain-america',
-    }, {
-      name: 'Spider-Man',
-      realname: 'Peter Parker',
-      firstappearance: 'Amazing Fantasy #15',
-      slug: 'spider-man',
-    }, {
-      name: 'Iron Man',
-      realname: 'Tony Stark',
-      firstappearance: 'Tales of Suspense #39',
-      slug: 'iron-man',
-    }, {
-      name: 'Thor',
-      realname: 'Thor Odinson',
-      firstappearance: 'Journey into Mystery #83',
-      slug: 'thor',
-    },
-  ]),
-
-  down: queryInterface => queryInterface.bulkDelete('heroes'),
-
+  up: async (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    await queryInterface.createTable('teams', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING },
+      slug: { type: Sequelize.STRING, unique: true },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+    return queryInterface.createTable('heroes', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING },
+      realname: { type: Sequelize.STRING },
+      firstappearance: { type: Sequelize.STRING },
+      slug: { type: Sequelize.STRING, unique: true },
+      teamId: { type: Sequelize.INTEGER, references: { model: 'teams', key: 'id' } },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+  },
+  down: async (queryInterface) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    await queryInterface.dropTable('heroes')
+    return queryInterface.dropTable('teams')
+  },
 }
